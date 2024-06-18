0879663bb20e05a69a85520052fe8a099110e47f
ecasallas07 <ecasallas07@gmail.com>
2024-06-18 17:24:35 -0500

    exmaples and image , resume topic data structures

--- code/Analysis_Complexity.py
+++ code/Analysis_Complexity.py
@@ -10,10 +10,37 @@ def test(n):
 
 
 
+# Exmaples analysys code
 
-def log(n):
-    i = n
-    for i in range(n):
-        i = i /2    
-        print(i)
-log(4)
+# Algortihm 1: Linear  Search
+
+def linear_search(arr,target):
+    for i in range(len(arr)): # --> time = O(n) record n attemps to array
+        if arr[i] == target: # --> time  = O(1) in the worst case not find the element
+            return i
+    return -1    # --> time = O(1)
+
+
+# Complexity Time = n  x 1 x 1 = O(n)
+
+
+# Algortihm 2: Binary Search
+
+def binar_search(array,traget):
+    
+    min = 0    # time --> O(1)
+    max = len(array) -1  # time  --> O(1)
+    
+    while min <= max: # --> time -->O(log n) in each iterattion divide for 2  // en cada iteration lo hace diviendo el array
+        mid = (min + max) // 2
+        if array[mid] == traget: # --> time O(1)
+            return mid
+        elif array[mid] < traget:   # --> time O(1)
+            min = mid + 1
+        elif array[mid] > traget: # --> time  O(1)
+            max = mid - 1
+            
+    return -1    
+    
+# complexity time 1 x 1 x 1 x 1 x log n = O(log n) Divide el array en la busqueda
+ 
