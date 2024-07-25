589bc04ec4c2b430a1bce889bdfc061bd74db344
ecasallas07 <ecasallas07@gmail.com>
2024-07-25 00:47:49 -0500

    practice project chatbot whatassap

---
+++ code/language/Chat_bot/app.py
@@ -0,0 +1,45 @@
+from re import search
+from flask import Flask, request
+from googlesearch import search as search_results
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+ 
+ 
+app = Flask(__name__)
+ 
+@app.route("/", methods=["POST"])
+ 
+# chatbot logic
+def bot():
+ 
+    # user input
+    user_msg = request.values.get('Body', '').lower()
+ 
+    # creating object of MessagingResponse
+    response = MessagingResponse()
+ 
+    # User Query
+    q = user_msg + "geeksforgeeks.org"
+ 
+    # list to store urls
+    result = []
+ 
+    # searching and storing urls
+    for i in search(q, num_results=3):
+        result.append(i)
+ 
+    # displaying result
+    msg = response.message(f"--- Results for '{user_msg}' ---")
+    for result in search_results:
+        msg = response.message(result)
+ 
+    return str(response)
+ 
+ 
+if __name__ == "__main__":
+    app.run()
+
+with request.test_request_context(
+    "/make_report/2017", query_string={"user_msg": "short"}
+):
+    bot()
