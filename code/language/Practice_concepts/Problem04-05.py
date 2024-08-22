ee4efe06754c1841aec4cd8b90a91045b3f77ff6
ecasallas07 <ecasallas07@gmail.com>
2024-08-21 23:39:40 -0500

    Solution Problems 04 - 06

--- code/language/Practice_concepts/Problem04-05.py
+++ code/language/Practice_concepts/Problem04-05.py
@@ -7,6 +7,8 @@
 """
 
 import re
+import os
+
 def calculate_interactive():
 
     view = []
@@ -17,40 +19,37 @@ def calculate_interactive():
         values.append(input_v)
         print(" ".join(view))
 
+
+        operators = []
+        numbers = []
         if input_v == '=':
 
-            # TODO: solution operation numbers and operators
-            operators = []
-            rr_op =[]
-            ar_num = []
             for i in values:
-                op = re.findall(r'[+\-/*]',i)
+
+                ope = re.findall(r'[+\-/*]',i)
                 num = re.findall(r'\d+',i)
-                if op:
-                    rr_op.append(op)
+
+
+                if ope:
+                    #se usa extend porque add al final
+                    operators.extend(ope)
+
                 if num:
-                    ar_num.append(num)
+                    numbers.extend(map(int,num))
 
-            for i in range(len(rr_op)):
-                count = 0
-                match rr_op[i]:
-                    case '+':
-                        if i == 0:
-                            count = ar_num.pop(i) + ar_num.pop(i+1)
-                        else:
-                            count += ar_num[]
+            result = numbers[0]
+            for i in range(len(operators)):
+                if operators[i] == '+':
+                    result += numbers[i+1]
+                elif operators[i] == '-':
+                    result -= numbers[i+1]
+                elif operators[i] == '*':
+                    result *= numbers[i+1]
+                elif operators[i] == '/':
+                    result /= numbers[i+1]
 
+            print(f"Result: {result}")
             break
 
 calculate_interactive()
 
-
-
-
-"""
-    Write a Python program that simulates a handheld calculator. Your pro-
-    gram should process input from the Python console representing buttons
-    that are “pushed,” and then output the contents of the screen after each op-
-    eration is performed. Minimally, your calculator should be able to process
-    the basic arithmetic operations and a reset/clear operation.
-"""
--- code/language/Practice_concepts/Problem06.py
+++ code/language/Practice_concepts/Problem06.py
@@ -5,3 +5,15 @@ following sentence one hundred times: “I will never spam my friends
 again.” Your program should number each of the sentences and it should
 make eight different random-looking typos.
 """
+import random
+def pnishment():
+    for i in range(100):
+        num = random.randint(1,8)
+        options = [3,4,10,15,23,49,34,12]
+        if i in options:
+            j= ['a',3,'d',10,'c',20,87,'ad','gb']
+            print(f" {i} I wi{j[1]} neve{j[num]}r spam my{j[num]} friends agai{j[3]}.")
+
+        print(f" {i} I will never spam my friends again.")
+
+pnishment()
