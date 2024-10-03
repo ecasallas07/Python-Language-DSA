2b85614aae5d8ec46c1ffe03da81e686f4c756e1
ecasallas07 <ecasallas07@gmail.com>
2024-10-02 23:46:07 -0500

    Progress script

--- code/projects/Automatizacion/Files_rename/main.py
+++ code/projects/Automatizacion/Files_rename/main.py
@@ -1,42 +1,42 @@
 import os
 import shutil
+import random
 
-# os.listdir('.')
+# 1. Modification of the directories
 
 name_os = os.name
 print('------------------------------------------------------------------------------')
-print(f'Welcome to {name_os} || Information routes and modified directorys and files')
+print(f'Welcome to {name_os} || Information routes and modified directorys ')
 print('------------------------------------------------------------------------------')
 
 print('Information your system: ')
 
+print(os.environ)
 shell = os.environ['SHELL']
 name_user = os.environ['USER']
 system_os =os.environ['XDG_SESSION_DESKTOP']
-shell_execution = os.environ['TERMINAL_EMULATOR']
 
 print(f"""
 Terminal : {shell[5:]}
 User : {name_user}
 System operative : {system_os}
-Terminal execution : {shell_execution}
 """)
 
 print('------------------------------------------------------------------------------')
 
-# print("""
-# Options:
-#
-# 1 = Renombrar archivos masivamente  = (pasar nombre de carpeta que contiene los archivos)
-# 2 = Mover archivos masivamente = (pasar nombre de directorio de origen y el de cambio)
-# 3 = ELiminar archivos masivamente = (pasar nombre de directorio)
-#
-# """)
+print("""
+Options:
+
+1 = Renombrar directorios masivamente  = (pasar nombre de carpeta que contiene los archivos)
+2 = Mover directorios masivamente = (pasar nombre de directorio de origen y el de cambio)
+3 = ELiminar directorios masivamente = (pasar nombre de directorio)
+
+""")
 
 action = int(input())
 
 
-if action==1:
+if action == 1:
     pass
     print('------------------------------------------------------------------------------')
     print("""
@@ -48,6 +48,7 @@ if action==1:
         4 --> Music
         5 --> Pictures
         6 --> videos
+
     """)
 
 
@@ -76,16 +77,17 @@ match options:
         print('Invalid option')
 
 
-# print(path)
+os.system('clear') # clear all in linux
+
 for file in os.listdir(path):
-    print(file)
+    if os.path.isdir(file):
+        fd=file
+        num = random.randint(1,10000)
+        os.rename(file, file+num)
+        print(file)
 
-# fd = "GFG.txt"
-# file = open(fd, 'r')
-# text = file.read()
-# print(text)
-# # os.close(file)
 
-# fd = "GFG.txt"
-# os.rename(fd,'New.txt')
 
+
+
+# 2. Modification of filesystem
